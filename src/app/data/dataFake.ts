ebb818237ac93a7d9810895787ad2f08fb36e24b
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2023-09-23 03:28:45 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Pão Doce",
+    "description": "Confira as receitas do nosso pão doce.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzJ5iL0BXMBPrEyFVsQ0QIv4JZWz3BRN2U2KGFaoXaczzvwFt662SZnZvERErFkOozEg4&usqp=CAU"
+  },
+  {
+    "id":"2",
+    "title": "Pão Salgado",
+    "description": "Não deixe de conferir nossas receitas",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1scoZhIKfeFM8DMF647J-ZNoXfjs-D8C6oA&usqp=CAU"
+  },
+  {
+    "id":"3",
+    "title": "Pão Árabe",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elitMaecenas ut nulla at dolor euismod aliquam nec ac nibh.Sed nec magna maximus, gravida orci lobortis, dictum neque.Nullam convallis velit eu arcu imperdiet, non consequat dui scelerisque.Nullam rutrum diam at diam posuere, in faucibus eros pulvinar.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR9BB7BWZiq9EZzQHcw7oAep12jZHTiJVqIFEEk9jUMhMPTJM8Eyx1ZteFOo0Odh9ixTw&usqp=CAU"
+  },
+  {
+    "id":"4",
+    "title": "Pão Ciabatta",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elitMaecenas ut nulla at dolor euismod aliquam nec ac nibh.Sed nec magna maximus, gravida orci lobortis, dictum neque.Nullam convallis velit eu arcu imperdiet, non consequat dui scelerisque.Nullam rutrum diam at diam posuere, in faucibus eros pulvinar.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8tIIBaF_uhKF9mnpB7oACmwmoW0ZjXP-gRQ&usqp=CAU"
+  }
+]
